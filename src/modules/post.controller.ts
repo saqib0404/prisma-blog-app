b08f05f1c8c707498e31f46e9eb29e7ed717b1dc
Saqib <saqibahmad0404@gmail.com>
2026-01-11 09:51:05 +0600

    admin seeding done

--- src/modules/post.controller.ts
+++ src/modules/post.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express"
 import { postService } from "./post.server"
+import { PostStatus } from "../../generated/prisma/enums"
 
 const createPost = async (req: Request, res: Response) => {
     try {
@@ -24,7 +25,22 @@ const getAllPosts = async (req: Request, res: Response) => {
     try {
         const { search } = req.query
         const searchString = typeof search === "string" ? search : undefined
-        const result = await postService.getAllPosts({ search: searchString })
+
+        const tags = req.query.tags ? (req.query.tags as string).split(",") : []
+
+        const isFeatured = req.query.isFeatured
+            ? req.query.isFeatured === "true"
+                ? true
+                : req.query.isFeatured === "false"
+                    ? false
+                    : undefined
+            : undefined
+
+        const status = req.query.status as PostStatus | undefined
+
+        const authorId = req.query.authorId as string | undefined
+
+        const result = await postService.getAllPosts({ search: searchString, tags, isFeatured, status, authorId })
         res.status(201).json(result)
     } catch (error: any) {
         res.status(400).json({
--- src/modules/post.server.ts
+++ src/modules/post.server.ts
@@ -1,4 +1,5 @@
-import { Post } from "../../generated/prisma/client";
+import { Post, PostStatus } from "../../generated/prisma/client";
+import { PostWhereInput } from "../../generated/prisma/models";
 import { prisma } from "../lib/prisma";
 
 const createPost = async (data: Omit<Post, 'id' | 'createdAt' | 'updatedAt' | 'authorId'>, userId: string) => {
@@ -11,28 +12,74 @@ const createPost = async (data: Omit<Post, 'id' | 'createdAt' | 'updatedAt' | 'a
     return result
 }
 
-const getAllPosts = async (payload: { search: string | undefined }) => {
-    const result = await prisma.post.findMany({
-        where: {
+const getAllPosts = async (
+    {
+        search,
+        tags,
+        isFeatured,
+        status,
+        authorId
+    }:
+        {
+            search?: string | undefined,
+            tags: string[] | [],
+            isFeatured: boolean | undefined,
+            status: PostStatus | undefined,
+            authorId: string | undefined
+        }
+) => {
+    const addContitions: PostWhereInput[] = []
+    if (search) {
+        addContitions.push({
             OR: [
                 {
                     title: {
-                        contains: payload.search as string,
+                        contains: search as string,
                         mode: "insensitive"
                     }
                 },
                 {
                     content: {
-                        contains: payload.search as string,
+                        contains: search as string,
                         mode: "insensitive"
                     }
                 },
                 {
                     tags: {
-                        has: payload.search as string
+                        has: search as string
                     }
                 }
             ]
+        })
+    }
+
+    if (tags.length > 0) {
+        addContitions.push({
+            tags: {
+                hasEvery: tags as string[]
+            }
+        })
+    }
+
+    if (typeof isFeatured === "boolean") {
+        addContitions.push({ isFeatured })
+    }
+
+    if (status) {
+        addContitions.push({
+            status
+        })
+    }
+
+    if (authorId) {
+        addContitions.push({
+            authorId
+        })
+    }
+
+    const result = await prisma.post.findMany({
+        where: {
+            AND: addContitions
         }
     });
     return result
